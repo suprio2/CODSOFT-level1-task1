35d3e7b27330018f1b9bddd02f8963303dc852ec
suprio2 <suprionaskar2@gmail.com>
2024-07-24 15:58:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar =  document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset =sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset+ height){
+            navlinks. forEach( link =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*'+ id + ']').classList.add('aactive')
+            })
+        }
+    })
+}
+
+
+menuIcon.onclick =() => {
+    menuIcon.classList.toggle( 'bx-x');
+    navbar.classList.toggle('active');
+
+}
